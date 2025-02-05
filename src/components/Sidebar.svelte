5c1504ee77d4ff13e3e2d3e989b94cf831b104b5
Tereza Knapkova <tereza.knapkova@gypri.cz>
2025-02-05 16:07:43 +0100

    define sidebar and add it to +layout.svelte

---
+++ src/components/Sidebar.svelte
@@ -0,0 +1,59 @@
+<script>
+    import fbLogo from '../assets/fb_logo.png';
+    import igLogo from '../assets/ig_logo.png';
+    import zvolsiinfoSidebar from '../assets/zvolsiingo_redLogo.png'
+    import "../global.css";
+</script>
+
+<div class="sidebar">
+    <div class="logo"><img src={zvolsiinfoSidebar} alt="Zvolsiinfo" /></div>
+    <div class="menu">
+        <span class="hamburger">☰</span>
+    </div>
+    <div class="social-icons">
+        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+            <img src={fbLogo} alt="Facebook" />
+        </a>
+        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <img src={igLogo} alt="Instagram" />
+        </a>
+    </div>
+</div>
+
+<style>
+    .sidebar {
+        color: var(--background-color);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: space-between;
+        padding: 20px;
+        height: 90%;
+        border-right: 0.5px solid rgba(0, 0, 0, 0.358);
+    }
+    .logo{
+        align-items: center;
+    }
+
+    .logo img {
+        width: 80%;
+    }
+
+    .social-icons {
+        display: flex;
+        gap: 10px;
+        flex-direction: column;
+    }
+
+    .social-icons img {
+        width: 24px;
+        height: 24px;
+    }
+
+    .hamburger {
+        cursor: pointer;
+        color: var(--my-gray);
+        font-size: 30px;
+
+    }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script>
+    import { onMount } from "svelte";
+    import Sidebar from "../components/Sidebar.svelte";
+</script>
+
+<div class="layout">
+    <aside class="sidebar">
+        <Sidebar />
+    </aside>
+
+    <main class="content">
+        <slot />
+    </main>
+</div>
+
+<style>
+    .layout {
+        display: flex;
+        height: 100vh;
+    }
+    .sidebar {
+        background: var(--background-color);
+        padding: 10px;
+        height:100%;
+    }
+    .content {
+        flex: 1;
+        padding: 20px;
+    }
+</style>
