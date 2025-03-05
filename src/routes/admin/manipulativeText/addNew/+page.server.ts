95ed7e410632ea99cf463899a408142d6eb15440
knapkova <118272531+knapkova@users.noreply.github.com>
2025-03-05 21:28:54 +0100

    feat: simplify load function in new assignment page by removing unused API call

--- src/routes/admin/manipulativeText/addNew/+page.server.ts
+++ src/routes/admin/manipulativeText/addNew/+page.server.ts
@@ -7,14 +7,13 @@ import { PUBLIC_VITE_API_ROOT } from '$env/static/public';
 
 
 const api_post = "/api/Admin/AssignmentManipulativeText/CreateAssignmentManipulativeText";
-const api_get = "/api/Admin/AssignmentManipulativeText/GetAssignmentManipulativeTexts";
 const newAssignmentSchema = z.object({
 	header: z.string().min(5, { message: 'min. 5 znaků' }),
 	text: z.string().min(20, { message: 'min. 20 znaků' }),
 	manipulativeParts: z.string().min(5, { message: 'zadejte alespoň jednu manipulativní část' }).default("[]")
 });
 
-export const load: PageServerLoad = async (event) => {
+export const load: PageServerLoad = async () => {
     const form = await superValidate(zod(newAssignmentSchema));
 	return { form };
 };
