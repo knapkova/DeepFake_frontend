9246790e4bc9479cef9d144b5caeb25a30bc7162
knapkova <118272531+knapkova@users.noreply.github.com>
2025-03-20 12:19:52 +0100

    feat: add registration functionality with form validation and error handling

---
+++ src/routes/account/register/+page.server.ts
@@ -0,0 +1,81 @@
+import { fail, message } from 'sveltekit-superforms';
+import { z } from 'zod';
+import { redirect } from '@sveltejs/kit';
+
+import { superValidate } from 'sveltekit-superforms/server';
+import type { Actions, PageServerLoad } from './$types';
+import { zod } from 'sveltekit-superforms/adapters';
+import { PUBLIC_VITE_API_ROOT } from '$env/static/public';
+
+
+const api_register = "/api/Security/Account/Register"
+
+
+const registerScheme = z
+  .object({
+    username: z.string().min(3, { message: 'min. 3 znaky' }),
+    email: z.string().email({ message: 'neplatný email' }),
+    password: z.string().min(6, { message: 'min. 6 znaků' }),
+    repeatedPassword: z.string()
+  })
+  .superRefine((data, ctx) => {
+    if (data.password !== data.repeatedPassword) {
+      ctx.addIssue({
+        code: z.ZodIssueCode.custom,
+        message: 'Hesla se neshodují',
+        path: ['repeatedPassword']
+      });
+    }
+  });
+
+export const load: PageServerLoad = async () => {
+    const form = await superValidate(zod(registerScheme));
+    return { form };
+}
+
+export const actions: Actions = {
+	default: async ({ request }: { request: Request }) => {
+		const form = await superValidate(request, zod(registerScheme));
+		console.log(form);
+		if (!form.valid) {
+			return fail(400, { form });
+		}
+		try {
+			const response = await fetch(`${PUBLIC_VITE_API_ROOT}${api_register}`, {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(form.data)
+			});
+			if (!response.ok) {
+				let errorData: { message?: string; errors?: string[] } = {};
+				try {
+					errorData = await response.json();
+				} catch (e) {
+					errorData.message = await response.text();
+				}
+				console.log('Error response:', errorData);
+				if (errorData.message === "Username is already in use.") {
+					form.errors.username = [errorData.message];
+				} else if (errorData.message === "Email is already in use.") {
+					form.errors.email = [errorData.message];
+				} else if (errorData.errors && Array.isArray(errorData.errors)) {
+					errorData.errors.forEach(err => {
+						if (err.includes("Username")) {
+							form.errors.username = [err];
+						}
+						if (err.includes("Email")) {
+							form.errors.email = [err];
+						}
+					});
+				}
+				return fail(response.status, { form });
+			}
+            console.log('Success response:', response);
+            return message(form, "Registrace proběhla úspěšně! :)");
+
+        		} catch (e) {
+			console.error(e);
+			return fail(500, { form });
+		}
+	}
+}
---
+++ src/routes/account/register/+page.svelte
@@ -0,0 +1,95 @@
+<script lang="ts">
+    import { enhance } from '$app/forms';
+import type { PageData } from './$types';
+import { superForm } from 'sveltekit-superforms';
+import { onMount } from 'svelte';
+import PasswordStrength from '../../../lib/PasswordStrength.svelte'
+import '../../../styles/form.css'
+
+export let data: PageData;
+
+const { form, errors, message } = superForm(data.form);
+
+
+
+onMount(async () => {
+  
+}); 
+
+</script>
+
+
+    <div>
+        {#if $message}
+        <div class="update-message">{$message} Nyní se můžete přihlásit <a href="/account/login">zde</a></div>
+        {/if}
+
+        <form use:enhance method="post" class="form-container">
+            <h2>Registrace</h2>
+            <p>Vyplňte prosím níže uvedený formulář pro vytvoření nového účtu.</p>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input 
+                    type="text" 
+                    id="username" 
+                    name="username" 
+                    required 
+                    bind:value={$form.username} 
+                    class="form-control" 
+                    placeholder="vložte uživatelské jméno"
+                    />
+                {#if $errors.username}
+                <div class="error-message">{$errors.username}</div>
+                {/if}
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input 
+                    type="email" 
+                    id="email" 
+                    name="email" 
+                    required 
+                    bind:value={$form.email} 
+                    class="form-control" 
+                    placeholder="vložte email"
+                    />
+                {#if $errors.email}
+                <div class="error-message">{$errors.email}</div>
+                {/if}
+            </div>
+            <div class="form-group">
+                <label for="password">Heslo</label>
+                <input 
+                    type="password" 
+                    id="password" 
+                    name="password" 
+                    required 
+                    bind:value={$form.password} 
+                    class="form-control" 
+                    placeholder="vložte heslo"
+                    />
+                {#if $errors.password}
+                <div class="error-message">{$errors.password}</div>
+                {/if}
+            </div>
+            <PasswordStrength password={$form.password} />
+            <div class="form-group">
+                <label for="repeatedPassword">Heslo znovu</label>
+                <input 
+                    type="password" 
+                    id="repeatedPassword" 
+                    name="repeatedPassword" 
+                    required 
+                    bind:value={$form.repeatedPassword} 
+                    class="form-control" 
+                    placeholder="potvrďte heslo"
+                    />
+                {#if $errors.repeatedPassword}
+                <div class="error-message">{$errors.repeatedPassword}</div>
+                {/if}
+                <button type="submit" class="btn-submit">Registrovat se</button>
+        </form>
+    </div>
+
+
+
