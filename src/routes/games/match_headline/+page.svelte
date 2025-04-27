bde5729efd7226a9fb212a0dc532b5f86d68f580
knapkova <118272531+knapkova@users.noreply.github.com>
2025-04-27 19:09:48 +0200

    feat: add feedback meme functionality; implement random meme fetching and display; update fun fact fetching logic

--- src/routes/games/match_headline/+page.svelte
+++ src/routes/games/match_headline/+page.svelte
@@ -2,24 +2,35 @@
     import MatchHeadline from "../../../assesments/matchHeadline.svelte";"../../assesments/matchHeadline.svelte";
     import EndQuiz from "../../../assesments/05_EndQuiz.svelte";
     import FindSource_dnd from "../../../assesments/03_FindSource.svelte";
-    import WheelOfEmotions from "$components/WheelOfEmotions.svelte";
     import CognitiveBias from "../../../assesments/04_CognitiveBias.svelte";
     import { tick } from "svelte";
-
+    import Motivation from "../../../assesments/02_motivations.svelte"
     import IntroductionText from "../../../assesments/01b_introductionText.svelte";
     import { onMount } from 'svelte';
     import { PUBLIC_VITE_API_ROOT } from "$env/static/public";
     import FunFactPopUp from "../../../components/FunFact.svelte"
+    import NewsletterPopUp from "../../../components/Newsletter_Popup.svelte"
+    import type {FunFact, FeedbackMeme} from "../../../types/interfaces.ts"
+    import { Button } from 'flowbite-svelte';
+    import { Tween } from 'svelte/motion';
+    import { cubicOut } from 'svelte/easing';
+    import "../../../styles/styles.css"
+	import Motivations from "../../../assesments/02_motivations.svelte";
 
-
-    import type {FunFact} from "../../../types/interfaces.ts"
     import { writable } from "svelte/store";
+    
+    const state_game = writable<1 | 2 | 3 | 4 | 5>(1);
+    let current_meme: FeedbackMeme | null = null;
     let funFactStore = writable<FunFact[]>([]);
+    let feedbackMemes = writable<FeedbackMeme[]>([]);
 
     let usedFunFacts: FunFact[] = [];
+    let usedFeedbackMemes: FeedbackMeme[] = [];
 
     let funfact_text = ""
     let funfact_header = ""
+
+
 
     async function getRandomFunFact() {
         showFunFact = false;
@@ -50,8 +61,43 @@
         return randomFunFact;
     }
 
+    async function get_random_meme(){
+      if ($feedbackMemes.length === 0) {
+        return null;
+      }
+      let randomIndex = Math.floor(Math.random() * $feedbackMemes.length);
+      let randomFeedbackMeme = $feedbackMemes[randomIndex];
+      if (usedFeedbackMemes.includes(randomFeedbackMeme)) {
+        console.log("Already used feedback meme, getting a new one");
+        if (usedFeedbackMemes.length === $feedbackMemes.length) {
+          console.log("All feedback memes have been used");
+          usedFeedbackMemes = [];
+        }
+        return get_random_meme();
+      }
+      usedFeedbackMemes.push(randomFeedbackMeme);
+      return randomFeedbackMeme;
+    }
+
+    async function get_feedbackmemes(){
+      let response = await fetch(PUBLIC_VITE_API_ROOT + "/api/Admin/FeedbackMeme/GetFeedbackMemes", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+      })
+
+      if (response.ok) {
+        const data = await response.json();
+        feedbackMemes.set(data);
+      } else {
+        console.error("Error fetching feedback memes:", response.statusText);
+      }
+    }
+
     async function get_funfacts(){
-      const category_id = 2;
+      const category_id = 11;
       let response = await fetch(PUBLIC_VITE_API_ROOT + "/api/Admin/FunFacts/GetFunFactsByCategoryId/" + category_id, {
         method: "GET",
         headers: {
@@ -71,16 +117,9 @@
         console.log('Component mounted');
         get_funfacts();
         console.log('Fun facts fetched');
-        funFactStore.subscribe((data) => {
-            console.log('Fun facts data:', data);
-        });
+        get_feedbackmemes();
+        console.log('Feedback memes fetched');    
     });
-
-
-    import { Button } from 'flowbite-svelte';
-    import { Tween } from 'svelte/motion';
-    import { cubicOut } from 'svelte/easing';
-    import "../../../styles/styles.css"
     
     let showFunFact = false;
     let progress = 100
@@ -115,13 +154,43 @@ $: animatedProgress.set(progress*6757);
     Randomize
   </Button>
 
-  <IntroductionText/>
-
   <Button on:click={() => getRandomFunFact()} class="mt-8">
     get fun fact random
   </Button>
 
   <FunFactPopUp showFunFact={showFunFact} header={funfact_header} text={funfact_text}/>
-    
+  
+  <Button on:click={async () => current_meme = await get_random_meme()} class="mt-8">
+    get feedback meme random
+  </Button>
+  
+  {#if current_meme}
+    <img src={current_meme.imgSrc} alt="Feedback Meme" class="w-1/2 mx-auto mt-4" />
+  {/if}
 </div>
+
+
+{#if $state_game === 1}
+    <IntroductionText />
+{/if}
+
+{#if $state_game === 2}
+    <Motivations/>
+{/if}
+
+{#if $state_game === 3}
+    <FindSource_dnd />
+{/if}
+
+{#if $state_game === 4}
+    <CognitiveBias />
+{/if}
+
+{#if $state_game === 5}
+    <EndQuiz />
+{/if}
+
+<Button on:click={() => state_game.update(n => (n < 5 ? (n + 1) as 1 | 2 | 3 | 4 | 5 : n))} class="mt-8">
+    Další levl
+  </Button>
 
