c12271195ceeab24071303e00ad5adde76df0f22
knapkova <118272531+knapkova@users.noreply.github.com>
2025-04-27 16:59:20 +0200

    feat: add initial .vscode/settings.json; update package.json and package-lock.json with new dependencies; modify vite.config.ts to include path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import path from 'path';
+
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [tailwindcss(), sveltekit()],
+	resolve: {
+		alias: {
+		  $assesments: path.resolve('./src/assesments'),
+		  $components: path.resolve('./src/components'),
+		  $types: path.resolve('./src/types'),
+		  $lib: path.resolve('./src/lib'),
+		  $styles: path.resolve('./src/styles'),
+		}
+	  }
 });
